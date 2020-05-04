0ee13bed532537e96d0e9d63daed6f0c67c02db4
clopezr9 <clopezr9@eafit.edu.co>
2020-05-03 21:17:26 -0500

    WebPage classes

---
+++ WebPage.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from zipfile import ZipFile
+from pathlib import Path
+import os
+import shutil
+
+class WebPage(ABC):
+    @abstractmethod
+    def __init__(self, name, creation_date, size, path):
+        pass
+
+
+class WebPagePDF(WebPage):
+    prefix = Path.home() / Path("Documents/Archie/PDF/")
+
+    def __init__(self, name, creation_date, size, path):
+        self.name = name
+        self.creation_date = creation_date
+        self.size = size
+        self.path = path
+        self.type = 'PDF'
+    
+
+class WebPageHTML(WebPage):
+    prefix = Path.home() / Path("Documents/Archie/HTML/")
+
+    def __init__(self, name, creation_date, size, path):
+        self.name = name
+        self.creation_date = creation_date
+        self.size = size
+        self.path = path
+        self.type = 'HTML'
+    
+
