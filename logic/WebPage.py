3ab76a94f6b5854cae55b6ccacda75b5c2dbac1e
Vincent A. Arcila <jandro.vins@gmail.com>
2020-05-05 22:20:30 -0500

    Solved bug

--- logic/WebPage.py
+++ logic/WebPage.py
@@ -14,7 +14,7 @@ class WebPagePDF(WebPage):
     prefix = Path.home() / Path("Documents/Archie/PDF/")
 
     def __init__(self, name, creation_date, size, path):
-        super(WebPagePDF, self).__init__()
+        super(WebPagePDF, self).__init__(name, creation_date, size, path)
         self.type = "PDF"
 
 
@@ -22,5 +22,5 @@ class WebPageHTML(WebPage):
     prefix = Path.home() / Path("Documents/Archie/HTML/")
 
     def __init__(self, name, creation_date, size, path):
-        super(WebPageHTML, self).__init__()
+        super(WebPageHTML, self).__init__(name, creation_date, size, path)
         self.type = "HTML"
