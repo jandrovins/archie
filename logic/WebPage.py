d13dc2202608d701b368c33eb571b18b00e3ec0f
Vincent Alejandro Arcila Larrea <jandro.vins@gmail.com>
2020-05-05 23:48:42 -0500

    Added View layer

--- logic/WebPage.py
+++ logic/WebPage.py
@@ -1,13 +1,19 @@
 from pathlib import Path
-import shutil
+from shutil import make_archive
 
 
 class WebPage:
-    def __init__(self, name, creation_date, size, path):
+    def __init__(self, name, creation_date, size, path, page_type='UNKNOWN'):
         self.name = name
         self.creation_date = creation_date
         self.size = size
         self.path = path
+        self.page_type = page_type
+
+    def compress(self):
+        zip_file = str(Path.home() / "Downloads" / webpage.name)
+        make_archive(zip_file, "zip", webpage.path)
+        
 
 
 class WebPagePDF(WebPage):
