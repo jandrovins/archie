8a1cdd74bf75fad08e1f492201e6c0eb6c694019
Vincent Alejandro Arcila Larrea <jandro.vins@gmail.com>
2020-05-05 21:46:41 -0500

    Reimplemented init method for WebPage's methods

--- logic/WebPage.py
+++ logic/WebPage.py
@@ -1,30 +1,26 @@
-from abc import ABC, abstractmethod
 from pathlib import Path
 import shutil
 
-class WebPage(ABC):
-    @abstractmethod
+
+class WebPage:
     def __init__(self, name, creation_date, size, path):
-        pass
+        self.name = name
+        self.creation_date = creation_date
+        self.size = size
+        self.path = path
 
 
 class WebPagePDF(WebPage):
     prefix = Path.home() / Path("Documents/Archie/PDF/")
 
     def __init__(self, name, creation_date, size, path):
-        self.name = name
-        self.creation_date = creation_date
-        self.size = size
-        self.path = path
-        self.type = 'PDF'
-    
+        super(WebPagePDF, self).__init__()
+        self.type = "PDF"
+
 
 class WebPageHTML(WebPage):
     prefix = Path.home() / Path("Documents/Archie/HTML/")
 
     def __init__(self, name, creation_date, size, path):
-        self.name = name
-        self.creation_date = creation_date
-        self.size = size
-        self.path = path
-        self.type = 'HTML'
+        super(WebPageHTML, self).__init__()
+        self.type = "HTML"
